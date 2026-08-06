df40977f8327b346a7f814ecade21362f70c4ed7
Dipesh Kumar <dipeshzzz47@gmail.com>
2026-08-06 21:11:36 +0530

    new dp problem added

--- DP/DP1 DP-1D Linear/198 - House Robber.cpp
+++ DP/DP1 DP-1D Linear/198 - House Robber.cpp
@@ -147,3 +147,210 @@ public:
         return next1;
     }
 };
+
+
+
+
+// House Robber II
+
+//==============================================================
+// LeetCode 213 - House Robber II
+// Pattern: Circular House Robber
+//
+// Idea:
+// Since first and last house are adjacent, we cannot rob both.
+//
+// Answer = max(
+//      Rob houses [0 ... n-2],
+//      Rob houses [1 ... n-1]
+// )
+//==============================================================
+
+
+
+/*
+--------------------------------------------------------------
+1. RECURSION
+Time  : O(2^n)
+Space : O(n)
+--------------------------------------------------------------
+*/
+
+class Solution {
+public:
+
+    int solve(int i, int end, vector<int>& nums)
+    {
+        if(i > end)
+            return 0;
+
+        int take = nums[i] + solve(i + 2, end, nums);  // Exclude last house
+
+        int notTake = solve(i + 1, end, nums);  // Exclude first house
+
+        return max(take, notTake);
+    }
+
+    int rob(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+
+        int case1 = solve(0, n - 2, nums);
+
+        int case2 = solve(1, n - 1, nums);
+
+        return max(case1, case2);
+    }
+};
+
+
+
+/*
+--------------------------------------------------------------
+2. MEMOIZATION (Top Down DP)
+Time  : O(n)
+Space : O(n)
+--------------------------------------------------------------
+*/
+
+class Solution {
+public:
+
+    int solve(int i, int end, vector<int>& nums, vector<int>& dp)
+    {
+        if(i > end)
+            return 0;
+
+        if(dp[i] != -1)
+            return dp[i];
+
+        int take = nums[i] + solve(i + 2, end, nums, dp); // Exclude last house
+
+        int notTake = solve(i + 1, end, nums, dp);  // Exclude first house
+
+        return dp[i] = max(take, notTake);
+    }
+
+    int rob(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+
+        int case1 = solve(0, n - 2, nums, dp1);
+
+        int case2 = solve(1, n - 1, nums, dp2);
+
+        return max(case1, case2);
+    }
+};
+
+
+
+/*
+--------------------------------------------------------------
+3. TABULATION (Bottom Up DP)
+Time  : O(n)
+Space : O(n)
+--------------------------------------------------------------
+*/
+
+class Solution {
+public:
+
+    int solve(vector<int>& nums, int start, int end)
+    {
+        int size = end - start + 1;
+
+        vector<int> dp(size + 2, 0);    
+
+        for(int i = size - 1; i >= 0; i--)
+        {
+            int take = nums[start + i] + dp[i + 2];
+
+            int notTake = dp[i + 1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[0];
+    }
+
+    int rob(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+
+        int case1 = solve(nums, 0, n - 2);
+
+        int case2 = solve(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};
+
+
+
+/*
+--------------------------------------------------------------
+4. SPACE OPTIMIZATION (Most Optimal)
+Time  : O(n)
+Space : O(1)
+--------------------------------------------------------------
+*/
+
+class Solution {
+public:
+
+    int solve(vector<int>& nums, int start, int end)
+    {
+        int next1 = 0;
+        int next2 = 0;
+
+        for(int i = end; i >= start; i--)
+        {
+            int take = nums[i] + next2;
+
+            int notTake = next1;
+
+            int curr = max(take, notTake);
+
+            next2 = next1;
+            next1 = curr;
+        }
+
+        return next1;
+    }
+
+    int rob(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+
+        int case1 = solve(nums, 0, n - 2);
+
+        int case2 = solve(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};
+
+
+
+
+
+
+
+
+
