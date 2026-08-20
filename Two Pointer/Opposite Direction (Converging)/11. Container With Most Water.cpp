fd1da77790c9b551043ddf2d8d15f269f6c99be9
Dipesh Kumar <dipeshzzz47@gmail.com>
2026-08-20 14:14:23 +0530

    11. leetcode

---
+++ Two Pointer/Opposite Direction (Converging)/11. Container With Most Water.cpp
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/dsa/container-with-most-water/
+// https://leetcode.com/problems/container-with-most-water/description/
+
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int maxWater(vector<int> &arr) {
+  	int left = 0, right = arr.size() - 1;
+    int res = 0;
+
+    while(left < right) {
+        
+        // find the water stored in the container between 
+        // arr[left] and arr[right]
+        int water = min(arr[left], arr[right]) * (right - left);      // AREA OF RECTANGLE = BASE * HEIGHT
+        res = max(res, water);
+      
+        if(arr[left] < arr[right])
+            left += 1;
+        else
+            right -= 1;
+    }
+  
+    return res;
+}
+
+int main() {
+    vector<int> arr = {2, 1, 8, 6, 4, 6, 5, 5};
+    cout << maxWater(arr);
+}
+
